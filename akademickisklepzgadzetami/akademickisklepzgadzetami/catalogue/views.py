aa9c84b443dd82b0007e740003cb1644933d995a
Oleksii Hudzishevskyi <oleksii.hudzishevskyi@gmail.com>
2026-01-07 18:50:41 +0100

    - fixing wciecia

--- akademickisklepzgadzetami/akademickisklepzgadzetami/catalogue/views.py
+++ akademickisklepzgadzetami/akademickisklepzgadzetami/catalogue/views.py
@@ -8,12 +8,13 @@ Product = get_model('catalogue', 'Product')
 
 def home(request):
     # 1. POBIERANIE Z BAZY
+    # Jeśli chcesz 12 produktów, zmień [:8] na [:12]
     db_products = Product.objects.filter(is_public=True).prefetch_related(
         'images',
         'stockrecords'
-    ).order_by('-date_created')
+    ).order_by('-date_created')[:12]
 
-    # Inicjalizacja strategii cenowej (niezbędne w Oscarze do poprawnych cen)
+    # Inicjalizacja strategii cenowej
     strategy = Selector().strategy(request=request, user=request.user)
 
     items = []
@@ -22,12 +23,12 @@ def home(request):
         # A. Obliczanie ceny i dostępności za pomocą strategii Oscara
         info = strategy.fetch_for_product(product)
 
+        # --- DOSTĘPNOŚĆ ---
         available_quantity = 0
-        # Sprawdzamy, czy produkt jest dostępny do kupienia i czy ma śledzony stan magazynowy
         if info.availability.is_available_to_buy:
             available_quantity = info.availability.num_available if info.availability.num_available else 0
-        # ----------------------------------------
 
+        # --- CENA ---
         price_val = '0'
         if info.price.is_tax_known:
             price_val = str(info.price.incl_tax)
@@ -35,27 +36,26 @@ def home(request):
             price_val = str(info.price.excl_tax)
 
         # B. Wyciąganie zdjęcia
-        # Jeśli produkt ma zdjęcie -> URL z media (np. /media/images/...)
-        # Jeśli nie -> URL ze static (np. /static/theme/images/...)
         img_obj = product.primary_image()
 
         if img_obj and img_obj.original:
             image_url = img_obj.original.url
         else:
-            # Budujemy pełną ścieżkę statyczną
             image_url = settings.STATIC_URL + 'theme/images/placeholder.png'
 
-            # C. Budowanie słownika
-            item = {
-                "id": str(product.id),
-                "name": product.title,
-                "image": image_url,
-                "price": price_val,
-                "quantity": available_quantity,
-                "promotion": 'false',
-                "discount": '0',
-                "discountedPrice": price_val
-            }
+        # C. Budowanie słownika
+        # WAŻNE: To wcięcie musi być równo z początkiem pętli for (4 spacje wewnątrz pętli),
+        # a NIE wewnątrz if-ów powyżej.
+        item = {
+            "id": str(product.id),
+            "name": product.title,
+            "image": image_url,
+            "price": price_val,
+            "quantity": available_quantity,
+            "promotion": 'false',
+            "discount": '0',
+            "discountedPrice": price_val
+        }
 
         items.append(item)
 
